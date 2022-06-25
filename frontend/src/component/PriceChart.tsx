058b27012359d661e41216b24ee93914f8699318
Chris Xiong <xionghc713@gmail.com>
2022-06-26 00:00:35 +0800

    update PriceChart with @mui Tabs

--- frontend/src/component/PriceChart.tsx
+++ frontend/src/component/PriceChart.tsx
@@ -1,36 +1,45 @@
 import * as React from "react";
 import axios from "axios";
+import { Tabs, Tab } from "@mui/material";
 import { url } from "../const/url";
 import { assetNames, currencyNames } from "../const/assets";
 import { Chart, LineAdvance } from "bizcharts";
 
-const query = {
-  asset: assetNames.BTC,
-  currency: currencyNames.USD,
-};
-
 export const PriceChart = () => {
   const [data, setData] = React.useState({});
+  const [tabValue, setTabValue] = React.useState(assetNames.BTC);
 
   React.useEffect(() => {
     axios
-      .get(`/market-history?asset=${query.asset}&currency=${query.currency}`, {
+      .get(`/market-history?asset=${tabValue}&currency=${currencyNames.USD}`, {
         baseURL: url.BASE_URL,
       })
       .then((res) => {
         setData(res.data.data);
       });
-  }, []);
+  }, [tabValue]);
+  
+  const handleChange = (event: React.SyntheticEvent, newTab: React.SetStateAction<assetNames>) => {
+    setTabValue(newTab);
+  };
 
   return (
-    <Chart padding={[20, 40, 100, 80]} autoFit height={700} data={data}>
-      <LineAdvance
-        shape="smooth"
-        point
-        area
-        position="time*price"
-        color="city"
-      />
-    </Chart>
+    <>
+      <Tabs
+        value={tabValue}
+        onChange={handleChange}
+      >
+        <Tab value={assetNames.BTC} label={assetNames.BTC} />
+        <Tab value={assetNames.ETH} label={assetNames.ETH} />
+      </Tabs>
+      <Chart padding={[20, 40, 100, 80]} autoFit height={700} data={data}>
+        <LineAdvance
+          shape="smooth"
+          point
+          area
+          position="time*price"
+        />
+      </Chart>
+    </>
   );
 };
